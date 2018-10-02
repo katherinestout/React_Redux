c65d35904637d5b466e95c11b0b46d8fbca08c35
katherinestout <kjs14@hood.edu>
2018-10-02 13:28:49 -0400

    store is available globally

---
+++ src/store/reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    age: 21
+};
+
+const reducer = (state =initialState , action) => {
+    const newState = {...state};
+
+    return newState;
+};
+
+export default reducer;
